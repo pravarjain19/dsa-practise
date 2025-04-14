d381b030c138b8868820d0cbd1516783f0c73388
Rimpymittal02 <rimppy.m@engineersmind.com>
2025-04-14 22:22:32 +0530

    Basic Recursion - 14-04-2025 (RE:2)

---
+++ Recursion/Basic Recursion/PrintNameFiiveTimes.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+public class PrintNameFiiveTimes
+{
+    public static void printNameFiveTimes(int count, int n)
+    {
+        if(count > n)
+        {
+            return;
+        }
+        //count++;
+        System.out.println("This is the name of a number fiive times");
+        printNameFiveTimes(count+1,n);
+    }
+    public static void printNumberNTimes(int count, int n)
+    {
+        if(count > n)
+        {
+            return;
+        }
+        //count++;
+        System.out.println("Value of count "+count);
+        printNumberNTimes(count+1,n);
+    }
+    public static void printNumberNTimesReverse(int count, int n)
+    {
+        if(count > n)
+        {
+            return;
+        }
+        printNumberNTimesReverse(count+1,n);
+        System.out.println("Value of count "+count);
+    }
+    public static void printNumberNTimesUsingBacktracking(int n)
+    {
+        if(n < 0)
+        {
+            return;
+        }
+        printNumberNTimesUsingBacktracking(n-1);
+        System.out.println("Value of count "+n);
+    }
+    public static void printNumberNTimesUsingBacktrackingReverse(int count, int n)
+    {
+        if(count > n)
+        {
+            return;
+        }
+        printNumberNTimesUsingBacktrackingReverse(count+1,n);
+        System.out.println("Value of count "+count);
+    }
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = input.nextInt();
+        System.out.println(n+" is a number fiive times");
+        //printNameFiveTimes(0,n);  //O(N) - TC, O(N) - SC
+        //printNumberNTimes(0,n);  //O(N) - TC, O(N) - SC
+        //printNumberNTimesReverse(0,n);  //O(N) - TC, O(N) - SC
+        //printNumberNTimesUsingBacktracking(n); //On the way forward, we go deeper.
+        //On the way backward (backtracking), we can print the numbers(keeping rest of the statements after the function call).
+        printNumberNTimesUsingBacktrackingReverse(0,n);
+    }
+}
